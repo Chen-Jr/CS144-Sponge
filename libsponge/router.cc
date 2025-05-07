04b2e2c3c8e29c378206fe1992125d8f64f0aad0
ChenJr <gejunchen7@gmail.com>
2025-05-07 12:01:09 +0000

    feature: finish lab6 and pass all the test

--- libsponge/router.cc
+++ libsponge/router.cc
@@ -15,9 +15,6 @@ using namespace std;
 
 // You will need to add private members to the class declaration in `router.hh`
 
-template <typename... Targs>
-void DUMMY_CODE(Targs &&... /* unused */) {}
-
 //! \param[in] route_prefix The "up-to-32-bit" IPv4 address prefix to match the datagram's destination address against
 //! \param[in] prefix_length For this route to be applicable, how many high-order (most-significant) bits of the route_prefix will need to match the corresponding bits of the datagram's destination address?
 //! \param[in] next_hop The IP address of the next hop. Will be empty if the network is directly attached to the router (in which case, the next hop address should be the datagram's final destination).
@@ -28,15 +25,37 @@ void Router::add_route(const uint32_t route_prefix,
                        const size_t interface_num) {
     cerr << "DEBUG: adding route " << Address::from_ipv4_numeric(route_prefix).ip() << "/" << int(prefix_length)
          << " => " << (next_hop.has_value() ? next_hop->ip() : "(direct)") << " on interface " << interface_num << "\n";
-
-    DUMMY_CODE(route_prefix, prefix_length, next_hop, interface_num);
-    // Your code here.
+    RouterInfo info = {route_prefix, prefix_length, next_hop, interface_num};
+    _router_list.push_back(info);
 }
 
 //! \param[in] dgram The datagram to be routed
 void Router::route_one_datagram(InternetDatagram &dgram) {
-    DUMMY_CODE(dgram);
-    // Your code here.
+    // if current ip datagram's ttl is less than 1, then drop it directly.
+    if (dgram.header().ttl <= 1) {
+        return;
+    }
+    uint32_t ip_dst = dgram.header().dst;
+    std::optional<RouterInfo> route_info = {};
+    size_t max_prefix = 0;
+    for (auto &it : _router_list) {
+        if ((static_cast<uint64_t>(ip_dst) >> (32 - it.prefix_length)) ==
+                (static_cast<uint64_t>(it.router_prefix) >> (32 - it.prefix_length)) &&
+            max_prefix <= it.prefix_length) {
+            max_prefix = it.prefix_length;
+            route_info.emplace(it);
+        }
+    }
+    if (route_info.has_value()) {
+        Address address = Address::from_ipv4_numeric(dgram.header().dst);
+        auto &net_interface = interface(route_info.value().interface_num);
+        dgram.header().ttl--;
+        if (route_info.value().next_hop.has_value()) {
+            address = route_info.value().next_hop.value();
+        }
+        net_interface.send_datagram(dgram, address);
+    }
+    return;
 }
 
 void Router::route() {
